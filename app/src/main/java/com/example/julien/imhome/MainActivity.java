226065d60645a0da5462400a986f3aeae5c13fdc
sideness <julien.stefani.fr@gmail.com>
2016-10-27 11:43:26 +0200

    Changement du titre de la MainActivity et ajout d'un petit encadré gris

--- app/src/main/java/com/example/julien/imhome/MainActivity.java
+++ app/src/main/java/com/example/julien/imhome/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        this.setTitle("Messages en attente d'envoi :");
+        this.setTitle("ImHome");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
